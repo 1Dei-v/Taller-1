05e4422a43f3a3f57e9a86c9b0f4ff919cfd851e
1Dei-v <dmunoz029@uan.edu.co>
2026-09-24 21:15:43 -0500

    Add files via upload

---
+++ 3.13.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip> //da formato numerico (no notacion cientifica)
+
+using namespace std;
+
+int main() {
+    printf("7.1\n");
+    printf("7.12\n");
+    printf("7.123\n");
+    printf("7.1234\n");
+    printf("7.12345\n");
+    printf("7.123456\n");
+
+    return 0;
+}
